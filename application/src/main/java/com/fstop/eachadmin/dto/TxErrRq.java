f77a6e26c54d71f6a7e3ed7040bf80cfe3d314ef
Andersen Lee <andersenlee@fstop.com.tw>
2022-09-16 17:08:09 +0800

    修改：TxErrRq

--- application/src/main/java/com/fstop/eachadmin/dto/TxErrRq.java
+++ application/src/main/java/com/fstop/eachadmin/dto/TxErrRq.java
@@ -1,5 +1,7 @@
 package com.fstop.eachadmin.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,7 +10,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class TxErrRq {
+public class TxErrRq<VW_ONBLOCKTAB> {
 	
 	@Schema(description = "營業日期")
 	@JsonProperty("BIZDATE")
@@ -21,5 +23,9 @@ public class TxErrRq {
 	@Schema(description = "")
 	@JsonProperty("PAGE")
 	private String page;
+	
+	@Schema(description = "")
+	@JsonProperty("ROWS")
+	private List<VW_ONBLOCKTAB> rows;
 	
 }
