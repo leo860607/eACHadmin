728256f9c6ec2e07af0a9ce16a934f77134c8df9
yuiweq1999 <yuiweq1999@gmail.com>
2022-09-20 16:35:02 +0800

    修改:Rq ,ignore

--- application/src/main/java/com/fstop/eachadmin/dto/DetailRq.java
+++ application/src/main/java/com/fstop/eachadmin/dto/DetailRq.java
@@ -29,43 +29,21 @@ public class DetailRq {
 	@JsonProperty("detailDataMap")
 	private Map detailDataMap;
 	
-	private Map DetailData;
-	private Map FILTER_BAT;
-	private String CLEARINGPHASE;
-	private String BIZDATE;
-	private String TXDT;
-	private String sourcePage;
-	private String EACHDT;
-	private String NEWTXAMT;
-	private String PCODE_DESC;
-	private String TXN_NAME;
-	private String TXN_TYPE;
-	private String SENDERBANK_NAME;
-	private String RECEIVERBANK_NAME;
-	private String SENDERID;
-	private String RECEIVERID;
-	private String INACCTNO;
-	private String INID;
-	private String OUTACCTNO;
-	private String OUTID;
-	private String SENDERBANKID_NAME;
+	private String EXTENDFEE;
+	private String NEWEXTENDFEE;
+	private String FEE_TYPE;
+	private String RESP;
+	private String NEWSENDERFEE_NW;
+	
 	private String NEWSENDERFEE;
-	private String INBANKID_NAME;
 	private String NEWINFEE;
-	private String OUTBANKID_NAME;
 	private String NEWOUTFEE;
-	private String WOBANKID_NAME;
 	private String NEWWOFEE;
-	private String REFUNDDEADLINE;
 	private String NEWEACHFEE;
-	private String TIMEOUTCODE;
-	private String NEWFEE;
-	private String FLBIZDATE;
-	private String FLBATCHSEQ;
-	private String FLPROCSEQ;
-	private String DATASEQ;
-	
-
-	
+	private String SENDERID;
+	private String TXN_NAME;
+	private String SENDERBANKID_NAME;
+	private String NEWTXAMT;
+	private String FILTER_BAT;
 	
 }
