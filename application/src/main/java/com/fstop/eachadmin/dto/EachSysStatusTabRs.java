d305f918a6e78bed3aefc1aa23a7c547beaf2aea
frank123 <a0916570526@gmail.com>
2022-09-20 10:15:43 +0800

    新增EachSysStatusTabRs&Rq,修改EachSysStatusTabService

---
+++ application/src/main/java/com/fstop/eachadmin/dto/EachSysStatusTabRs.java
@@ -0,0 +1,22 @@
+package com.fstop.eachadmin.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+@Getter
+@Setter
+public class EachSysStatusTabRs<EACHSYSSTATUSTAB> {
+
+	@Schema(description = "")
+	@JsonProperty("result")
+	private String result;
+	
+	@Schema(description = " ")
+	@JsonProperty("msg")
+	private String msg;
+}
