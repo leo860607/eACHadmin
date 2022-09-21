57e721f371ff700a8183b015a76de45ef435aa44
CHEN Y.A <gift60307@gmail.com>
2022-09-21 10:03:12 +0800

    新增：TxErrDetailService

--- application/src/main/java/com/fstop/eachadmin/dto/TxErrDetailRs.java
+++ application/src/main/java/com/fstop/eachadmin/dto/TxErrDetailRs.java
@@ -1,5 +1,22 @@
 package com.fstop.eachadmin.dto;
 
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TxErrDetailRs {
+	
+	private String NEWEXTENDFEE;
+	private String TXN_TYPE;
+	private String NEWSENDERFEE_NW;
+	private String NEWINFEE_NW;
+	private String NEWOUTFEE_NW;
+	private String NEWWOFEE_NW;
+	private String NEWEACHFEE_NW;
+	private String NEWFEE_NW;
+	private Map DetailData;
 
 }
