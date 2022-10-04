9820cb6c8018b220191f469b21b801c965ea97a1
frank123 <a0916570526@gmail.com>
2022-10-04 14:35:09 +0800

    總行代號API

--- application/src/main/java/com/fstop/eachadmin/service/BankGroupService.java
+++ application/src/main/java/com/fstop/eachadmin/service/BankGroupService.java
@@ -1,69 +1,55 @@
 package com.fstop.eachadmin.service;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fstop.eachadmin.dto.BankGroupRq;
-import com.fstop.eachadmin.dto.BankGroupRs;
 import com.fstop.fcore.util.StrUtils;
 import com.fstop.infra.entity.BANK_OPBK;
-import com.fstop.eachadmin.service.BankOpbkService;
+import lombok.extern.slf4j.Slf4j;
 import util.DateTimeUtils;
 import util.zDateHandler;
 @Service
-
+@Slf4j
 public class BankGroupService {
 	@Autowired
 	private BankOpbkService bank_opbk_bo;
-	public BankGroupRs getByOpbkId_Single_Date(BankGroupRq params){
-		String paramName;
+	public List<Map<String, String>> getByOpbkId_Single_Date(BankGroupRq params){
 		String paramValue;
-		List<BANK_OPBK> list  = null;
 		Map rtnMap = new HashMap();
 		String OPBK_ID = "";
-		//paramName = "OPBK_ID";
 		paramValue = params.getOPBK_ID();
 		if (StrUtils.isNotEmpty(paramValue)){
 			OPBK_ID = paramValue;
 		}
 		String s_bizdate = "";
-		paramName = "s_bizdate";
 		paramValue = params.getS_bizdate();
 		if (StrUtils.isNotEmpty(paramValue)){
-			s_bizdate = paramValue;
-		}else{
-			rtnMap.put("result", "FALSE");
-			rtnMap.put("msg", "營業日期不可空白");
-			ObjectMapper mapper = new ObjectMapper();
-			BankGroupRs result = mapper.convertValue(rtnMap, BankGroupRs.class);
-			System.out.println(result);
-			return result;
-		}
+			s_bizdate = paramValue;}
 		rtnMap = zDateHandler.verify_BizDate(DateTimeUtils.convertDate(1, s_bizdate, "yyyyMMdd", "yyyyMMdd"));
-		if(rtnMap.get("result").equals("FALSE")){
-			ObjectMapper mapper = new ObjectMapper();
-			BankGroupRs result = mapper.convertValue(rtnMap, BankGroupRs.class);
-			System.out.println(result);
-			return result;
-		}
-		list  = bank_opbk_bo.getCurBgbkList(OPBK_ID, s_bizdate);
+		log.debug(DateTimeUtils.convertDate(1, s_bizdate, "yyyyMMdd", "yyyyMMdd"));
+		log.debug(rtnMap.toString());
+		List<BANK_OPBK> list = bank_opbk_bo.getCurBgbkList(OPBK_ID, s_bizdate);;
+		List<Map<String, String>> beanList = new LinkedList<Map<String, String>>();
+		Map<String, String> bean = null;
+		for (BANK_OPBK po : list) {
 			
-		if(list != null && list.size() > 0){
-			rtnMap.put("result", "TRUE");
-			rtnMap.put("msg", list);
-		}else{
-			rtnMap.put("result", "FALSE");
-			rtnMap.put("msg", "無總行資料");
+			String k1 = "BGBKName";
+			String v1 = (String) po.getBGBK_NAME();
+			
+			String k2 = "BGBKId";
+			String v2 = (String) po.getBGBK_ID();
+			
+			bean = new HashMap<String, String>();
+			bean.put(k1, v1);
+			bean.put(k2, v2);
+			beanList.add(bean);
 		}
-//		result = JSONUtils.map2json(rtnMap);
-		ObjectMapper mapper = new ObjectMapper();
-		BankGroupRs result = mapper.convertValue(rtnMap, BankGroupRs.class);
-		System.out.println(result);
-		return result;
-	}	
+		System.out.println("beanList>>" + beanList);
+		return beanList;
+	}
 }
--- application/src/main/java/com/fstop/eachadmin/service/BankOpbkService.java
+++ application/src/main/java/com/fstop/eachadmin/service/BankOpbkService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Service;
 
 import com.fstop.eachadmin.repository.BankOpbkRepository;
@@ -15,14 +14,13 @@ import util.StrUtils;
 public class BankOpbkService {
 	@Autowired
 	private BankOpbkRepository bank_opbk_Dao;
-	@SuppressWarnings("unchecked")
 	public List<BANK_OPBK> getCurBgbkList(String opbk_id ,String s_bizdate  ){
 		List<BANK_OPBK> list = null;
 		String sqlPath = ""; 
-		PreparedStatementSetter param = (PreparedStatementSetter) new HashMap<String, String>();
+		Map<String,String> param =  new HashMap<String, String>();
 		if(StrUtils.isNotEmpty(opbk_id)){
-			sqlPath = " WHERE OPBK_ID = :opbk_id ";
-			((HashMap<String,String>) param).put("opbk_id", opbk_id);
+			sqlPath = "WHERE OPBK_ID = :opbk_id ";
+			param.put("opbk_id", opbk_id);
 		}
 		try{
 			list = bank_opbk_Dao.getCurBgbkList(sqlPath ,param, s_bizdate);
