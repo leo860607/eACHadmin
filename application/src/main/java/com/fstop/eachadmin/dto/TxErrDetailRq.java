d753482e8ed7be14f32a13f48daef69f2d88eef6
Andersen Lee <andersenlee@fstop.com.tw>
2022-10-04 14:07:03 +0800

    修改：TxErrDetail

--- application/src/main/java/com/fstop/eachadmin/dto/TxErrDetailRq.java
+++ application/src/main/java/com/fstop/eachadmin/dto/TxErrDetailRq.java
@@ -26,16 +26,16 @@ public class TxErrDetailRq {
 	@JsonProperty("target")
 	private	String	target;
 
-	private String EXTENDFEE;
-	private String FEE_TYPE;
-	private String RESP;
-	private String NEWSENDERFEE;
-	private String NEWINFEE;
-	private String NEWOUTFEE;
-	private String NEWWOFEE;
-	private String NEWEACHFEE;
-	private String NEWFEE;
-	private String SENDERBANKID_NAME;
-	private String TXN_NAME;
+//	private String EXTENDFEE;
+//	private String FEE_TYPE;
+//	private String RESP;
+//	private String NEWSENDERFEE;
+//	private String NEWINFEE;
+//	private String NEWOUTFEE;
+//	private String NEWWOFEE;
+//	private String NEWEACHFEE;
+//	private String NEWFEE;
+//	private String SENDERBANKID_NAME;
+//	private String TXN_NAME;
 	
 }
