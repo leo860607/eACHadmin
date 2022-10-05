543f63f6a64cd682f17e5127dc086c4af9b69cbb
WuJayUi <famousoneman@gmail.com>
2022-10-05 09:15:58 +0800

    delete undoneDetailService

--- application/src/main/java/com/fstop/eachadmin/controller/UndoneController.java
+++ application/src/main/java/com/fstop/eachadmin/controller/UndoneController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fstop.eachadmin.service.UndoneDetailService;
 import com.fstop.eachadmin.service.UndoneService;
 import com.fstop.infra.entity.UNDONE_TXDATA;
 import com.fstop.eachadmin.dto.DetailRq;
